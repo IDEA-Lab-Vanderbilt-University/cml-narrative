4e460e36127af8c9715944ffa3f95be8c09d32c2
Ian Thompson <ianthompson@nicelion.com>
2022-09-01 16:20:05 -0400

    Created new SvelteKit app

---
+++ application/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<!--
+ +layout.svelte
+ application
+ 
+ Created by Ian Thompson on September 1st 2022
+ icthomp@g.clemson.edu
+ 
+ https://idealab.sites.clemson.edu
+ 
+--->
+<script>
+	import '../app.css';
+</script>
+
+<slot />
