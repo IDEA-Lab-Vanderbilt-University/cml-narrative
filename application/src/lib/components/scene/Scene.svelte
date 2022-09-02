1d48817af014d4acf2d3e0d50a69c09a41bb34f0
Ian Thompson <ianthompson@nicelion.com>
2022-09-02 12:13:21 -0400

    Initial commit

---
+++ application/src/lib/components/scene/Scene.svelte
@@ -0,0 +1,26 @@
+<!--
+ Scene.svelte
+ application
+ 
+ Created by Ian Thompson on September 2nd 2022
+ icthomp@g.clemson.edu
+ 
+ https://idealab.sites.clemson.edu
+ 
+--->
+<script>
+	/**  The background image of the scene */
+	export let background = '';
+</script>
+
+<div class="w-screen h-screen">
+	<div class="bg-gray-400 w-full h-full relative z-0">
+		<div
+			class="bg-center bg-cover z-0 w-full h-full"
+			style={`background-image: url('${background}');`}
+		/>
+		<div class="absolute inset-x-0 bottom-0 mb-16 flex justify-center items-center z-10">
+			<slot />
+		</div>
+	</div>
+</div>
--- application/src/routes/+page.svelte
+++ application/src/routes/+page.svelte
@@ -1,58 +1,18 @@
-<script>
-	import DialogBox from '../components/DialogBox.svelte';
+<!--
+ +page.svelte
+ application
+ 
+ Created by Ian Thompson on September 1st 2022
+ icthomp@g.clemson.edu
+ 
+ https://idealab.sites.clemson.edu
+ 
+--->
 
-	const script = [
-		{
-			speaker: 'Captain Storm',
-			dialog: 'Welcome to the Solving Problems of Tomorrow Agency, my name is Captain Storm. '
-		},
-		{
-			speaker: 'Captain Storm',
-			dialog: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit.'
-		},
-		{
-			speaker: 'Captain Apple',
-			dialog:
-				'Facilis harum soluta maiores possimus recusandae placeat animi ratione pariatur eos qui non quam commodi, quasi sed eaque accusamus hic facere earum minima natus laborum molestiae ut itaque!'
-		},
-		{
-			speaker: 'Captain Storm',
-			dialog: 'Incidunt voluptatum quod veniam.'
-		}
-	];
+<div class="p-8 flex flex-col">
+	<h1 class="text-4xl text-center">Welcome to the CML SvelteKit Demo</h1>
 
-	var scriptState = 0;
-
-	var currentLine = script[0];
-
-	const handleDialogEvent = (event) => {
-		var state = event.detail.state;
-
-		console.log(state);
-
-		if (state == 'forward' && scriptState < script.length - 1) {
-			scriptState = scriptState + 1;
-			currentLine = script[scriptState];
-		} else if (state == 'back' && scriptState > 0) {
-			scriptState = scriptState - 1;
-
-			currentLine = script[scriptState];
-		}
-	};
-</script>
-
-<div class="w-screen h-screen">
-	<div class="bg-gray-400 w-full h-full relative z-0">
-		<div
-			class="bg-center bg-cover z-0 w-full h-full"
-			style="background-image: url('img/principals-office-with-desk.jpg');"
-		/>
-		<div class="absolute inset-x-0 bottom-0 mb-16 flex justify-center items-center z-10">
-			<DialogBox
-				on:dialogEvent={handleDialogEvent}
-				speaker={currentLine.speaker}
-				dialog={currentLine.dialog}
-			/>
-		</div>
-	</div>
+	<a href="/demos/simple-script" class="h-24 w-fit rounded-md bg-blue-400 p-10 text-black text-2xl"
+		>Simple Script Example</a
+	>
 </div>
