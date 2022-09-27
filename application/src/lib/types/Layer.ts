951f8c9e438ec3a6a0786b1e05322a5607db88e1
Ian Thompson <ianthompson@nicelion.com>
2022-09-27 16:13:09 -0400

    Adds canvas to project

---
+++ application/src/lib/types/Layer.ts
@@ -0,0 +1,27 @@
+/**
+ Layer.ts
+ application
+ 
+ Created by Ian Thompson on September 9th 2022
+ icthomp@g.clemson.edu
+ 
+ https://idealab.sites.clemson.edu
+ 
+*/
+
+export interface Layer {
+    scene: CanvasRenderingContext2D,
+    height: number
+    width: number
+    x: number
+    y: number
+
+    image?: HTMLImageElement
+    
+    (scene: CanvasRenderingContext2D)
+    
+    update()
+
+    draw(context: CanvasRenderingContext2D)
+    
+}
