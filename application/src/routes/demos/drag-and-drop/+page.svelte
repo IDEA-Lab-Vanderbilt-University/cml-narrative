7d640dfa697f52c7f4c144307420e149334c2457
Ian Thompson <ianthompson@nicelion.com>
2022-10-18 16:36:32 -0400

    remove old code

--- application/src/routes/demos/drag-and-drop/+page.svelte
+++ application/src/routes/demos/drag-and-drop/+page.svelte
@@ -9,52 +9,6 @@
  
 --->
 
-<!-- <script lang="ts">
-    import {flip} from "svelte/animate";
-    import {dndzone} from "svelte-dnd-action";
-    let items = [
-        {id: 1, name: "item1"},
-        {id: 2, name: "item2"},
-        {id: 3, name: "item3"},
-        {id: 4, name: "item4"}
-    ];
-    const flipDurationMs = 300;
-    function handleDndConsider(e) {
-        items = e.detail.items;
-    }
-    function handleDndFinalize(e) {
-        items = e.detail.items;
-    }
-</script>
-
-<!-- <div class="text-black bg-white h-screen">
-    hello
-</div> -->
-
-<!-- <style>
-    section {
-        width: 100%;
-        padding: 0.3em;
-        border: 1px solid black;
-        /* this will allow the dragged element to scroll the list */
-        overflow: scroll;
-        height: 200px;
-    }
-    div {
-        width: 100%;
-        padding: 0.2em;
-        border: 1px solid blue;
-        margin: 0.15em 0;
-    }
-</style>
-
-<div class="text-black bg-white h-screen w-full">
-    <section use:dndzone="{{items, flipDurationMs}}" on:consider="{handleDndConsider}" on:finalize="{handleDndFinalize}">
-        {#each items as item(item.id)}
-        <div animate:flip="{{duration: flipDurationMs}}">{item.name}</div>
-        {/each}
-    </section>
-</div> -->
 
 <script>
 	import Bank from "$lib/components/dropzone/Bank.svelte";
