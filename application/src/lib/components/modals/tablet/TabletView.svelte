cfc480e17aa815be032ae3da30f6f4a9ebd32051
Ian Thompson <ianthompson@nicelion.com>
2022-10-18 16:36:25 -0400

    Initial commit

---
+++ application/src/lib/components/modals/tablet/TabletView.svelte
@@ -0,0 +1,37 @@
+<!--
+ TabletView.svelte
+ cml-narrative
+ 
+ Created by Ian Thompson on October 18th 2022
+ icthomp@g.clemson.edu
+ 
+ https://idealab.sites.clemson.edu
+ 
+--->
+
+<div class="bg-independence w-full h-screen">
+    <div class="flex justify-between items-center p-4">
+        <!-- img -->
+        <!-- <img src="/img/logos/SPOT-logo.svg" class="w-1/6" alt=""> -->
+        <p class="text-6xl font-bold text-white">S.P.O.T</p>
+        <!-- Tagline -->
+        <p class="text-3xl font-bold text-white">Solving Problems of Tomorrow</p>
+    </div>
+
+    <div class="w-full h-4/5 p-8">
+        <!-- content -->
+        <div class="h-full w-full bg-gray-50 rounded-md p-2">
+            <slot />
+        </div>
+        
+    </div>
+    <div class="flex justify-between px-10 items-center">
+        <!-- footer -->
+        <img src="/img/icons/settings.png"  class="h-20 w-20 hover:animate-wiggle" alt="">
+        <div class="">
+            MEGA JOULES METER?
+        </div>
+        <img src="/img/icons/power.png" class="h-20 w-20 hover:animate-wiggle" alt="">
+
+    </div>
+</div>
