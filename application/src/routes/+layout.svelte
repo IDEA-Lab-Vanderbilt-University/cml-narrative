fc7d875625a5ec296808b8663b1222a8709a38cc
Ian Thompson <ianthompson@nicelion.com>
2023-01-07 13:38:30 -0500

    Create new project using kit 1.0

---
+++ application/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<!--
+ /src/routes/+layout.svelte
+ +layout.svelte
+ cml-narrative
+ 
+ Created by Ian Thompson on January 7th 2023
+ icthomp@g.clemson.edu
+ 
+ https://idealab.sites.clemson.edu
+
+ 
+ This file serves as the main layout handler. Any code present on this page will be present on all pages on this web application. 
+
+ The "slot" component is dynamic, as other components can be fed into it. 
+
+--->
+
+<script>
+    import "../app.css";
+</script>
+  
+<slot />
