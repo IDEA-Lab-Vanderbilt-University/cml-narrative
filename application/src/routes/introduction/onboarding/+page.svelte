09bf0888512ad09851df474a93125a576374985f
Ian Thompson <ianthompson@nicelion.com>
2023-01-08 08:28:50 -0500

    update text

--- application/src/routes/introduction/onboarding/+page.svelte
+++ application/src/routes/introduction/onboarding/+page.svelte
@@ -22,7 +22,7 @@
         <Tablet>
             <div class="flex flex-col w-full h-full justify-center items-center text-white bg-gray-100 space-y-8">
                 <h1 class="text-6xl text-jet">Welcome, new SPOT Agent!</h1>
-                <a href="#" class=" bg-sky-900 text-2xl px-4 py-5 rounded-full  font-mokoto shadow-md hover:shadow-lg hover:opacity-75 transition ease-in-out duration-200">Create Agent Profile</a>
+                <a href="#" class=" bg-sky-900 text-2xl px-6 py-5 rounded-full  font-mokoto shadow-md hover:shadow-lg hover:opacity-75 transition ease-in-out duration-200">Create Agent Profile</a>
             </div>
         </Tablet>
     </div>
