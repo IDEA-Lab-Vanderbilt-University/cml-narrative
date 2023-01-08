a8fedd65360592a20037b7bc1fcdf94bd79486b9
Ian Thompson <ianthompson@nicelion.com>
2023-01-07 23:33:18 -0500

    Added matrix rainfall

--- application/src/routes/+page.svelte
+++ application/src/routes/+page.svelte
@@ -13,26 +13,108 @@
 <!-- Default landing page -->
 
 <script lang="ts">
+	import DialogBox from "$lib/components/dialog/DialogBox.svelte";
 	import Scene from "$lib/components/scene/Scene.svelte";
 	import Tablet from "$lib/components/tablet/Tablet.svelte";
+	
+	import { onMount }  from 'svelte';;
+	import { fade, fly } from 'svelte/transition';
 
+	let showTablet: boolean = false
+
+	onMount(() => {
+		// Initialising the canvas
+		var canvas = document.querySelector('canvas')
+		var ctx = canvas.getContext('2d');
+
+		// // Setting the width and height of the canvas
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+
+		// // Setting up the letters
+		var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXYZABCDEFGHIJKLMNOPQRSTUVXY123456789';
+		letters = letters.split('');
+
+		// Setting up the columns
+		var fontSize = 10,
+			columns = canvas.width / fontSize;
+
+		// Setting up the drops
+		var drops = [];
+		for (var i = 0; i < columns; i++) {
+			drops[i] = 1;
+		}
+
+
+		// Setting up the draw function
+		function draw() {
+			ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+			ctx.fillRect(0, 0, canvas.width, canvas.height);
+			for (var i = 0; i < drops.length; i++) {
+
+				var text = letters[Math.floor(Math.random() * letters.length)];
+				ctx.fillStyle = '#0f0';
+				ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+				drops[i]++;
+
+				if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+					drops[i] = 0;
+					showTablet = true
+
+				}
+			}
+		}
+
+		// Loop the animation
+		setInterval(draw, 33);
+	})
+
+	
 
 </script>
 
-<Scene background="/img/backgrounds/Spark_Lab.jpg">
-	<div class="w-full h-full p-2" slot="content">
-		<Tablet></Tablet>
-		<!-- <div class="bg-white h-full rounded p-8 flex flex-col items-center justify-center">
-			<div class="text-black space-y-5">
-				<h1 class="text-center text-6xl">Welcome, agent!</h1>
-				<p class="text-xl">
-					Welcome to the SPOT Agency portal. Please select an option below
-				</p>
-			</div>
-			<div class="flex flex-col space-y-8 mt-12 w-full items-center">
-				<a href="/introduction?page=1" class="text-2xl rounded p-4 bg-blue-400 text-white shadow-md hover:shadow-lg w-full text-center max-w-lg">New Agent</a>
-				<a href="/" class="text-2xl rounded p-4 bg-blue-400 text-white shadow-md hover:shadow-lg w-full text-center max-w-lg">Returning Agent</a>
-			</div>
-		</div> -->
+
+
+<div class="w-full h-screen bg-gray-200 flex justify-center items-center">
+	<div class="bg-gray-400 h-full w-full relative z-0">
+		<canvas />
+		{#if showTablet}
+		<div class="absolute inset-0 flex justify-center items-center z-10 p-48" in:fade="{{delay: 500}}">
+			<Tablet >
+				<div class="bg-white h-full rounded p-8 flex flex-col items-center justify-center" >
+					<div class="text-black space-y-5">
+						<h1 class="text-center text-6xl">Welcome, agent!</h1>
+						<p class="text-xl">
+							Welcome to the SPOT Agency portal. Please select an option below.
+						</p>
+					</div>
+					<div class="flex flex-col space-y-8 mt-12 w-full items-center">
+						<a href="/introduction?page=1" class="text-2xl rounded p-4 bg-blue-400 text-white shadow-md hover:shadow-lg w-full text-center max-w-lg">New Agent</a>
+						<a href="/" class="text-2xl rounded p-4 bg-blue-400 text-white shadow-md hover:shadow-lg w-full text-center max-w-lg">Returning Agent</a>
+					</div>
+				</div>
+			</Tablet>
+		</div>
+
+		{/if}
 	</div>
-</Scene>
+  </div>
+
+<!-- <Scene background="/img/backgrounds/Spark_Lab.jpg">
+	<div class="w-full h-full py-6 px-40" slot="content">
+		<Tablet>
+			<div class="bg-white h-full rounded p-8 flex flex-col items-center justify-center">
+				<div class="text-black space-y-5">
+					<h1 class="text-center text-6xl">Welcome, agent!</h1>
+					<p class="text-xl">
+						Welcome to the SPOT Agency portal. Please select an option below
+					</p>
+				</div>
+				<div class="flex flex-col space-y-8 mt-12 w-full items-center">
+					<a href="/introduction?page=1" class="text-2xl rounded p-4 bg-blue-400 text-white shadow-md hover:shadow-lg w-full text-center max-w-lg">New Agent</a>
+					<a href="/" class="text-2xl rounded p-4 bg-blue-400 text-white shadow-md hover:shadow-lg w-full text-center max-w-lg">Returning Agent</a>
+				</div>
+			</div>
+		</Tablet>
+	</div>
+</Scene> -->
