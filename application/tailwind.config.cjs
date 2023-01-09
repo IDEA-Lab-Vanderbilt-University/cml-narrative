8248349f56857f253e47de0f639bfde7d1b9cf14
Ian Thompson <ianthompson@nicelion.com>
2023-01-09 12:22:55 -0500

    Reformetted and added new styles

--- application/tailwind.config.cjs
+++ application/tailwind.config.cjs
@@ -1,27 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        cantora: ["Cantora One"],
-		mokoto: ["Mokoto"]
-      },
-      colors: {
-				peach: "#ffbd59",
-				jet: "#3e3c3c",
-				independence: "#585f76"
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				cantora: ['Cantora One'],
+				mokoto: ['Mokoto']
 			},
-      keyframes: {
+			colors: {
+				peach: '#ffbd59',
+				jet: '#3e3c3c',
+				independence: '#585f76',
+				"lapiz-blue": "#29638C"
+			},
+			keyframes: {
 				wiggle: {
 					'0%, 100%': { transform: 'rotate(-10deg)' },
 					'50%': { transform: 'rotate(10deg)' }
-				},
+				}
 			},
 			animation: {
-				wiggle: 'wiggle 0.8s ease-in-out 4',
+				wiggle: 'wiggle 0.8s ease-in-out 4'
 			}
-    },
-  },
+		}
+	},
+	daisyui: {
+		themes: ['light']
+	},
 	plugins: [require('daisyui')]
-}
+};
