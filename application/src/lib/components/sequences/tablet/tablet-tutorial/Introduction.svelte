b02162b59a750935235bcf38bb55b9f9826dc537
Ian Thompson <ianthompson@nicelion.com>
2023-01-14 14:02:49 -0500

    Initial commit

---
+++ application/src/lib/components/sequences/tablet/tablet-tutorial/Introduction.svelte
@@ -0,0 +1,26 @@
+<!--
+ /src/lib/components/sequences/tablet/tablet-tutorial/Introduction.svelte
+ Introduction.svelte
+ cml-narrative
+ 
+ Created by Ian Thompson on January 14th 2023
+ icthomp@g.clemson.edu
+ 
+ https://idealab.sites.clemson.edu
+ 
+--->
+
+<div class="flex justify-around items-center font-mokoto text-lapiz-blue p-8 align-middle w-full h-full">
+    <div class="flex flex-col items-center justify-center space-y-7 text-2xl">
+        <img src="/img/icons/journal.png" alt="" class="h-56">
+        <p>Travel Log</p>
+    </div>
+    <div class="flex flex-col items-center justify-center space-y-7 text-2xl">
+        <img src="/img/icons/profile.png" alt="" class="h-56">
+        <p>Profile</p>
+    </div>
+    <div class="flex flex-col items-center justify-center space-y-7 text-2xl">
+        <img src="/img/icons/award.png" alt="" class="h-56">
+        <p>Badges</p>
+    </div>
+</div>
---
+++ application/src/lib/components/sequences/tablet/tablet-tutorial/SpotApplication.svelte
@@ -0,0 +1,20 @@
+<!--
+ /src/lib/components/sequences/tablet/tablet-tutorial/TravelLog.svelte
+ TravelLog.svelte
+ cml-narrative
+ 
+ Created by Ian Thompson on January 14th 2023
+ icthomp@g.clemson.edu
+ 
+ https://idealab.sites.clemson.edu
+ 
+--->
+<script lang="ts">
+    export let img: string
+    export let title: string
+</script>
+
+<div class="flex flex-col items-center justify-center space-y-7 text-2xl w-full h-full font-mokoto text-lapiz-blue">
+    <img src={img} alt="" class="h-56">
+    <p>{title}</p>
+</div>
