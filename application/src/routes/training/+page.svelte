2f66bf6751ff69baa85bfb6c7489126d2707fcdc
Ian Thompson <ianthompson@nicelion.com>
2023-01-16 17:40:03 -0500

    Begin pulldown view

--- application/src/routes/training/+page.svelte
+++ application/src/routes/training/+page.svelte
@@ -10,3 +10,28 @@
  
 --->
 
+<script lang="ts">
+	import Scene from "$lib/components/scene/Scene.svelte";
+    import { fade, fly } from 'svelte/transition';
+
+
+</script>
+
+<Scene background="/img/backgrounds/Spark_Lab.jpg">
+    <div class="w-full h-full" slot="content">
+        <div class="flex w-full h-full flex-col" >
+            <div class="w-full h-12 bg-gray-900 z-10"></div>
+            <div class="h-full z-0" in:fly="{{ y: -800, duration: 2000 }}">
+                <div class="bg-white mx-8 h-4/5 rounded-sm border-4 border-b-0 border-black border-t-0">d</div>
+                <div class="w-full h-12 bg-white rounded-sm border-4 border-black"></div>
+                <div class="w-full flex items-center justify-center flex-col">
+                    <div class="w-2 bg-black h-12"></div>
+                    <div class="h-10 w-10 rounded-full bg-lapiz-blue border-4 border-black"></div>
+                </div>
+            </div>
+            
+        </div>
+
+
+    </div>
+</Scene>
