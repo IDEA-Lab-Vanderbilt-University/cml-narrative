b9bc0425c8f61d51aa0118722db254390b972e12
Ian Thompson <ianthompson@nicelion.com>
2023-01-16 22:30:13 -0500

    update

--- demo/src/lib/activities/drag-and-drop.svelte
+++ demo/src/lib/activities/drag-and-drop.svelte
@@ -38,38 +38,39 @@
     }
 
 </script>
-    <div class="h-full w-full bg-gray-100 p-4">
-        <div class="text-black space-y-2">
-            <h1 class="text-3xl text-center">Are these technologies helpful or harmful?</h1>
-            <p class="text-xl">Drag and drop each technology into one of the baskets below based on your thoughts on whether the
-                technology is helpful or harmful?</p>
-        </div>
-    
-        <div class="flex flex-col justify-around space-y-10">
-            <div class="mt-4 h-1/4">
-                <Bank items={items1} type='light'/>
-            </div>
 
-            <div class="flex space-x-3 h-full">
-                <div class="h-full w-full text-center text-2xl text-black bg-gray-50 p-2 border-2 rounded-sm">
-                    <p>Harmful</p>
-                    <Bank items={hurtful} type='light' id="harmful" on:itemDropped={handleDrop}/>
-        
-                </div>
-                <div class=" h-full w-full text-center text-2xl text-black bg-gray-50 p-2 border-2 rounded-sm">
-                    <p>Helpful</p>
-                    <Bank items={helpful} type='light'id="helpful" on:itemDropped={handleDrop}/>
-        
-                </div>
-                <div class=" h-full w-full text-center text-2xl text-black bg-gray-50 p-2 border-2 rounded-sm">
-                    <p>Both Harmful and Helpful</p>
-                    <Bank items={bothHurtfulAndHelpful} type='light' id="harmfulAndHelpful" on:itemDropped={handleDrop}/>
-        
-                </div>
-        
-            </div>
-        </div>
-
+<div class="h-full w-full bg-gray-100 p-4">
+    <div class="text-black space-y-2">
+        <h1 class="text-3xl text-center">Are these technologies helpful or harmful?</h1>
+        <p class="text-xl">Drag and drop each technology into one of the baskets below based on your thoughts on whether the
+            technology is helpful or harmful?</p>
     </div>
+
+    <div class="flex flex-col justify-around space-y-10">
+        <div class="mt-4 h-1/4">
+            <Bank items={items1} type='light'/>
+        </div>
+
+        <div class="flex space-x-3 h-full">
+            <div class="h-full w-full text-center text-2xl text-black bg-gray-50 p-2 border-2 rounded-sm">
+                <p>Harmful</p>
+                <Bank items={hurtful} type='light' id="harmful" on:itemDropped={handleDrop}/>
+    
+            </div>
+            <div class=" h-full w-full text-center text-2xl text-black bg-gray-50 p-2 border-2 rounded-sm">
+                <p>Helpful</p>
+                <Bank items={helpful} type='light'id="helpful" on:itemDropped={handleDrop}/>
+    
+            </div>
+            <div class=" h-full w-full text-center text-2xl text-black bg-gray-50 p-2 border-2 rounded-sm">
+                <p>Both Harmful and Helpful</p>
+                <Bank items={bothHurtfulAndHelpful} type='light' id="harmfulAndHelpful" on:itemDropped={handleDrop}/>
+    
+            </div>
+    
+        </div>
+    </div>
+
+</div>
 
 
