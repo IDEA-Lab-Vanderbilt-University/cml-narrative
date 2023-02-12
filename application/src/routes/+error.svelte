086fe0da980bba11580194d83ba63589aad9046f
Ian Thompson <ianthompson@nicelion.com>
2023-02-12 17:50:53 -0500

    added error page

---
+++ application/src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<!--
+ /src/routes/+error.svelte
+ +error.svelte
+ cml-narrative
+ 
+ Created by Ian Thompson on February 12th 2023
+ icthomp@g.clemson.edu
+ 
+ https://idealab.sites.clemson.edu
+ 
+--->
+
+<script lang="ts">
+    import { page } from '$app/stores';
+</script>
+
+<div class="w-full h-screen flex items-center justify-center flex-col space-y-6">
+    <h1 class="text-4xl">Sorry, Agent!</h1>
+    <p>This was not supposed to happen. Please alert an adult to this error!</p>
+    <p>{$page.status}: {$page.error?.message}</p>
+</div>
