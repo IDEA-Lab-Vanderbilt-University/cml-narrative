fed33343f3ff6fcec60c7d7fd53de62e61c4d4cf
Ian Thompson <ianthompson@nicelion.com>
2023-06-27 12:32:04 -0400

    added asset manifest

---
+++ application/src/lib/utils/Assets/Badges.ts
@@ -0,0 +1,12 @@
+/**
+ /src/lib/utils/AssetManifest/Badges.ts
+ Badges.ts
+ cml-narrative
+ 
+ Created by Ian Thompson on June 27th 2023
+ icthomp@g.clemson.edu
+ 
+ https://idealab.sites.clemson.edu
+ 
+*/
+
---
+++ application/src/lib/utils/Assets/index.ts
@@ -0,0 +1,17 @@
+/**
+ /src/lib/utils/Assets/index.ts
+ index.ts
+ cml-narrative
+ 
+ Created by Ian Thompson on June 27th 2023
+ icthomp@g.clemson.edu
+ 
+ https://idealab.sites.clemson.edu
+ 
+*/
+
+import { Characters } from "./Characters";
+
+export const Assets = {
+    Characters
+}
