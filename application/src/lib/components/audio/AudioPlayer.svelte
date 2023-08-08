13e7c86b4802e61d6dfd1c736f40f24996752f86
Ian Thompson <ianthompson@nicelion.com>
2023-08-08 11:40:02 -0400

    begin adding audio player

---
+++ application/src/lib/components/audio/AudioPlayer.svelte
@@ -0,0 +1,25 @@
+<!--
+ /src/lib/components/audio/AudioPlayer.svelte
+ AudioPlayer.svelte
+ cml-narrative
+ 
+ Created by Ian Thompson on August 8th 2023
+ icthomp@g.clemson.edu
+ 
+ https://idealab.sites.clemson.edu
+ 
+--->
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let src: string;
+	let player: HTMLAudioElement;
+
+	onMount(() => {
+		player.play();
+	});
+</script>
+
+<audio bind:this={player} {src}>
+	<track kind="captions" />
+</audio>
