a7fcef4857263d9dc537fa21ea2d64eaa3d05dd2
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2023-12-13 14:28:09 -0500

    dnd-and-reasoning-fixed

--- application/src/lib/components/activities/drag-and-drop/DNDTextResponse.svelte
+++ application/src/lib/components/activities/drag-and-drop/DNDTextResponse.svelte
@@ -12,24 +12,14 @@
 <script lang="ts">
 	import DataService from '$lib/utils/DataService';
 	import { createEventDispatcher } from 'svelte';
-	import { harmfulHelpfulStore } from '$lib/utils/stores/store';
-	import type { HarmfulHelpfulItem, HarmfulHelpfulReasoningItem } from '$lib/types/DragDropItem';
-	// /**
-	//  * The id attribute is used for when we save data to the backend.
-	//  */
-	// export let id: string;
+	import { dragItemsStore, harmfulHelpfulStore } from '$lib/utils/stores/store';
+	import type { DragStackItem, Reasoning } from '$lib/types/DragDropItem';
 
-	/**
-	 * The prompted technology for which you want the user to respond to.
-	 * For now, this is just AI and ML as responses.
-	 */
-	// export let promptedTechnology: string;
-	export let harmfulProp: HarmfulHelpfulItem;
+	export let currentDragObject: DragStackItem;
+	export let response: string = '';
 
 	let dispatch = createEventDispatcher();
 	let recognition: any;
-
-	export let response: string = '';
 
 	const speechToText = () => {
 		// @ts-ignore
@@ -70,25 +60,24 @@
 	const handleSubmit = async () => {
 		console.log(response);
 		// store command
-		harmfulHelpfulStore.update((value) => {
-			// let currentReasoning: HarmfulHelpfulReasoningItem = [
-			// 	{
-			// 		// title: harmfulProp.title,
-			// 		// itemId: harmfulProp.itemId,
-			// 		id: harmfulProp.id,
-			// 		type: harmfulProp.type,
-			// 		reasonText: response,
-			// 		reasonVideo: ''
-			// 	}
-			// ];
-			value.reasoning.push({
-				id: harmfulProp.id,
-				type: harmfulProp.type,
-				reasonText: response,
-				reasonVideo: ''
-			});
-			return value;
+		// harmfulHelpfulStore.update((value) => {
+		// 	value.reasoning.push({
+		// 		id: currentDragObject.id,
+		// 		type: currentDragObject.type,
+		// 		reasonText: response,
+		// 		reasonVideo: ''
+		// 	});
+		// 	return value;
+		// });
+
+		let data: Reasoning = { reasonText: response, reasonVideo: '' };
+		dragItemsStore.update((items) => {
+			items[0].reasoning = data;
+			console.log('handlesubmit lo ', items);
+			return items;
 		});
+
+		// update the harmfulHelpfulStore
 
 		response = '';
 		dispatch('textDone');
@@ -98,11 +87,13 @@
 <div
 	class="flex h-full w-full flex-col items-center justify-center rounded-md bg-gray-900 bg-opacity-50 p-5 font-mono text-white bg-blend-darken">
 	<!-- <h1 class="text-4xl font-bold">What do you think {promptedTechnology} is?</h1> -->
-	<h1 class="text-4xl font-bold">Why do you think {harmfulProp.title} is {harmfulProp.type}?</h1>
+	<h1 class="text-4xl font-bold">
+		Why do you think {currentDragObject.title} is {currentDragObject.type}?
+	</h1>
 	<p class="mt-4 text-xl italic">In the box below, describe why your reasoning.</p>
 	<textarea
 		class="textarea-bordered textarea  mx-12 mt-9 h-1/2 w-full border-4 border-dashed border-white bg-transparent text-xl"
-		placeholder={`I think ${harmfulProp.title} is ${harmfulProp.type} because...`}
+		placeholder={`I think ${currentDragObject.title} is ${currentDragObject.type} because...`}
 		bind:value={response} />
 	<button class="bg-lapiz-blue mt-9 rounded-md px-8 text-xl text-white" on:click={handleSubmit}
 		>Next</button>
--- application/src/routes/activities/harmful-or-helpful-reasoning/+page.svelte
+++ application/src/routes/activities/harmful-or-helpful-reasoning/+page.svelte
@@ -1,99 +1,132 @@
 <script lang="ts">
 	import Tablet from '$lib/components/tablet/Tablet.svelte';
 	import DNDResponse from '$lib/components/activities/drag-and-drop/DNDResponse.svelte';
-	import { goto } from '$app/navigation';
-	import { harmfulHelpfulStore } from '$lib/utils/stores/store';
-	import DataService from '$lib/utils/DataService';
-	import type { HarmfulHelpfulItem, HarmfulHelpfulStoreItem } from '$lib/types/DragDropItem';
+	import { onMount } from 'svelte';
+	import type { DragStackItem, HarmfulHelpfulItem } from '$lib/types/DragDropItem';
+	import { dragItemsStore, harmfulHelpfulStore } from '$lib/utils/stores/store';
+	// import { goto } from '$app/navigation';
+	// import { harmfulHelpfulStore } from '$lib/utils/stores/store';
+	// import DataService from '$lib/utils/DataService';
+	// import type { HarmfulHelpfulItem, HarmfulHelpfulStoreItem } from '$lib/types/DragDropItem';
 
-	const nextItem = () => {
-		if (currentItemIndex < harmfulProps.length - 1) {
-			currentItemIndex += 1;
-		} else {
-			handleTotalHarmFulHelpfulSubimission();
-			goto('/training?page=5');
-		}
-	};
-
-	const handleTotalHarmFulHelpfulSubimission = async () => {
-		let data: any;
-		harmfulHelpfulStore.subscribe((value) => {
-			data = value;
-		});
-		try {
-			await DataService.Data.submitHelpfulOrHarmfulResponse(data);
-		} catch (error) {
-			console.error(error);
-		}
-	};
-
-	// const handleSubmit = async () => {
-	// 	try {
+	// const nextItem = () => {
+	// 	if (currentItemIndex < harmfulProps.length - 1) {
+	// 		currentItemIndex += 1;
+	// 	} else {
+	// 		handleTotalHarmFulHelpfulSubimission();
 	// 		goto('/training?page=5');
+	// 	}
+	// };
+
+	// const handleTotalHarmFulHelpfulSubimission = async () => {
+	// 	let data: any;
+	// 	harmfulHelpfulStore.subscribe((value) => {
+	// 		data = value;
+	// 	});
+	// 	try {
+	// 		await DataService.Data.submitHelpfulOrHarmfulResponse(data);
 	// 	} catch (error) {
 	// 		console.error(error);
 	// 	}
 	// };
 
-	const handleVideoSubmission = async (event: any) => {
-		try {
-			console.log(event.detail);
-			// fetch s3 url from the media endpoint
-			// you get url, item id, itemId, from the event.detail
-			// some store logic if there isn't
-			console.log('video submssion started!');
-			nextItem();
-		} catch (error) {
-			console.error(error);
-		}
-	};
+	// // const handleSubmit = async () => {
+	// // 	try {
+	// // 		goto('/training?page=5');
+	// // 	} catch (error) {
+	// // 		console.error(error);
+	// // 	}
+	// // };
 
-	const generateHarmfulProps = (harmfulHelpfulObject: HarmfulHelpfulStoreItem) => {
-		let result: HarmfulHelpfulItem[] = [];
+	// const handleVideoSubmission = async (event: any) => {
+	// 	try {
+	// 		console.log(event.detail);
+	// 		// fetch s3 url from the media endpoint
+	// 		// you get url, item id, itemId, from the event.detail
+	// 		// some store logic if there isn't
+	// 		console.log('video submssion started!');
+	// 		nextItem();
+	// 	} catch (error) {
+	// 		console.error(error);
+	// 	}
+	// };
 
-		harmfulHelpfulObject.harmful.forEach((item) => {
-			result.push({
-				id: item.id,
-				itemId: item.itemId,
-				title: item.title,
-				type: 'harmful'
-			});
+	// const generateHarmfulProps = (harmfulHelpfulObject: HarmfulHelpfulStoreItem) => {
+	// 	let result: HarmfulHelpfulItem[] = [];
+
+	// 	harmfulHelpfulObject.harmful.forEach((item) => {
+	// 		result.push({
+	// 			id: item.id,
+	// 			itemId: item.itemId,
+	// 			title: item.title,
+	// 			type: 'harmful'
+	// 		});
+	// 	});
+
+	// 	harmfulHelpfulObject.helpful.forEach((item, id) => {
+	// 		result.push({
+	// 			id: item.id, // Continue the index from the end of the harmful array
+	// 			itemId: item.itemId,
+	// 			title: item.title,
+	// 			type: 'helpful'
+	// 		});
+	// 	});
+
+	// 	result.sort((a, b) => a.id - b.id);
+
+	// 	return result;
+	// };
+
+	// let harmfulHelpfulObject: HarmfulHelpfulStoreItem = {
+	// 	harmful: [],
+	// 	helpful: [],
+	// 	reasoning: []
+	// };
+	// harmfulHelpfulStore.subscribe((value) => {
+	// 	harmfulHelpfulObject = value;
+	// });
+
+	// console.log('harmfulobject: ', harmfulHelpfulObject);
+
+	// let harmfulProps = generateHarmfulProps(harmfulHelpfulObject);
+	// console.log(harmfulProps);
+
+	// let currentItemIndex = 0;
+	let currentDragObject: DragStackItem;
+
+	onMount(() => {
+		dragItemsStore.subscribe((value) => {
+			currentDragObject = value[0];
 		});
-
-		harmfulHelpfulObject.helpful.forEach((item, id) => {
-			result.push({
-				id: item.id, // Continue the index from the end of the harmful array
-				itemId: item.itemId,
-				title: item.title,
-				type: 'helpful'
-			});
-		});
-
-		result.sort((a, b) => a.id - b.id);
-
-		return result;
-	};
-
-	let harmfulHelpfulObject: HarmfulHelpfulStoreItem = {
-		harmful: [],
-		helpful: [],
-		reasoning: []
-	};
-	harmfulHelpfulStore.subscribe((value) => {
-		harmfulHelpfulObject = value;
 	});
 
-	console.log('harmfulobject: ', harmfulHelpfulObject);
+	const nextItem = () => {
+		let actaulHOHItem: HarmfulHelpfulItem = {
+			id: currentDragObject.id,
+			itemId: currentDragObject.itemId,
+			title: currentDragObject.title,
+			type: currentDragObject.type,
+			reasoning: currentDragObject.reasoning
+		};
 
-	let harmfulProps = generateHarmfulProps(harmfulHelpfulObject);
-	console.log(harmfulProps);
+		harmfulHelpfulStore.update((value) => {
+			value.push(actaulHOHItem);
+			console.log('harmfulHelpfulStore: ', value);
+			return value;
+		});
 
-	let currentItemIndex = 0;
+		dragItemsStore.update((items) => items.slice(1));
+	};
+
+	const handleVideoSubmission = () => {
+		console.log('video submitted');
+		nextItem();
+	};
 </script>
 
 <Tablet>
 	<DNDResponse
-		harmfulProp={harmfulProps[currentItemIndex]}
+		{currentDragObject}
 		on:nextItem={nextItem}
 		on:videosSubmitted={handleVideoSubmission} />
 </Tablet>
