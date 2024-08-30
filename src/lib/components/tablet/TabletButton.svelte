1d6c52228c61eb59725146046770b83001e9b462
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2024-08-30 14:11:07 -0500

    Refactor tablet button

---
+++ src/lib/components/tablet/TabletButton.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+    const dispatch = createEventDispatcher();
+
+    function handleClick() {
+        dispatch('click');
+    }
+</script>
+
+
+<div class="flex w-full ">
+    <button
+        on:click={handleClick}
+        class="mx-4 my-4 ml-auto w-fit  rounded-full bg-green-500 px-6 py-7 shadow-md hover:shadow-lg">
+        <img
+            src="/img/icons/mobile-app.png"
+            alt=""
+            class="h-24 animate-pulse hover:animate-none" />
+    </button>
+</div>
