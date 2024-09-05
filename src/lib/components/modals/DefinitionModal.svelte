972e209cefa3792e5a06d4dff877ccff8816b836
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2024-09-05 16:52:28 -0500

    Definition view

---
+++ src/lib/components/modals/DefinitionModal.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import DefinitionView from "../tablet/DefinitionView.svelte";
+	import Tablet from "../tablet/Tablet.svelte";
+
+    export let title: string;
+    export let definition: string;
+</script>
+
+<Tablet>
+    <DefinitionView title={title} definition={definition} />
+
+    <div class="button-container">
+        <button class="button" on:click>Next</button>
+    </div>
+</Tablet>
+
+<style>
+.button-container {
+    display: flex;
+    justify-content: center;
+}
+
+.button {
+    background-color: #4adde2;
+    border: none;
+    color: white;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 1.5rem;
+    padding: 10px 24px;
+    cursor: pointer;
+    border-radius: 30px;
+}
+
+</style>
