5dc81c33959f28d7742fb1a22ea9a9352c2b7426
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2024-09-05 16:00:15 -0500

    More of level 1 script

--- src/lib/scripts/level1/index.ts
+++ src/lib/scripts/level1/index.ts
@@ -64,9 +64,48 @@ const script: Script = {
             pos: 'center',
             size: 'full',
         },
-
+        {
+            id: 7,
+            speakers: ['Bot Buddy'],
+            dialog:
+                'These robots must have been trained with an algorithm on how to make and serve pizza!',
+            avatars: ['/img/characters/bot-buddy/bot-buddy-happy.png'],
+            background: '/img/backgrounds/level1/cafe.png',
+            audio: '/audio/level1/bot_buddy/bot_buddy_l1s7.wav',
+            pos: 'left'
+        },
+        {
+            id: 8,
+            speakers: ['Bot Buddy'],
+            dialog:
+                'Remember what an algorithm is? Let\'s go back to your training notes in your SPOT tablet.',
+            avatars: ['/img/characters/bot-buddy/bot-buddy-happy-2.png'],
+            background: '/img/backgrounds/level1/cafe.png',
+            audio: '/audio/level1/bot_buddy/bot_buddy_l1s8.wav',
+            pos: 'left'
+        },
+        {
+            id: 9,
+            speakers: ['Bot Buddy'],
+            dialog:
+                'My database says that an algorithm is a set of step-by-step instructions that describe how to perform a task. It is kind of like a recipe!',
+            avatars: ['/img/characters/bot-buddy/bot-buddy-happy.png'],
+            background: '/img/backgrounds/level1/cafe.png',
+            audio: '/audio/level1/bot_buddy/bot_buddy_l1s9.wav',
+            pos: 'left'
+        },
+        {
+            id: 10,
+            speakers: ['Bot Buddy'],
+            dialog:
+                'Let\'s go into the kitchen and see if we can use an algorithm to make our own pizzas.',
+            avatars: ['/img/characters/bot-buddy/bot-buddy-happy-2.png'],
+            background: '/img/backgrounds/level1/cafe.png',
+            audio: '/audio/level1/bot_buddy/bot_buddy_l1s10.wav',
+            pos: 'left'
+        },
 	],
-	length: 7
+	length: 10
 };
 
 export default script;
