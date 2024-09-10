4915065ce34ee88715c58ea153b4c1d5f4e18d02
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2024-09-10 15:32:32 -0500

    Start tablet menu

---
+++ src/lib/components/tablet/TabletMenu.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+
+
+    export let onSelect: (selection: null | "profile" | "travelLog" | "badges") => void;
+
+</script>
+
+
+
+<div>
+    Profiles
+    Travel Log
+    Badges
+</div>
+
+<style>
+    div {
+        background-color: rgba(0, 0, 0, 0.5);
+        color: white;
+        padding: 1rem;
+        border-radius: 1rem;
+    }
+</style>
