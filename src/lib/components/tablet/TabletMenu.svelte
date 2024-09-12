1c32bfc9ae827bbdfeebe0d6dac0c6de3d2df67c
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2024-09-12 10:39:05 -0500

    More menu icon work

--- src/lib/components/tablet/TabletMenu.svelte
+++ src/lib/components/tablet/TabletMenu.svelte
@@ -1,23 +1,40 @@
 <script lang="ts">
+	import { Assets } from "$lib/utils/Assets";
+	import SpotApplication from "../sequences/tablet/tablet-tutorial/SpotApplication.svelte";
+
 
 
     export let onSelect: (selection: null | "profile" | "travelLog" | "badges") => void;
+    let handleAppContainerEvent = (e: CustomEvent<{ event: string; id: string }>) => {
+         
+    };
 
 </script>
 
 
 
-<div>
-    Profiles
-    Travel Log
-    Badges
+<div class="flex justify-center" id="tablet-menu">
+    <SpotApplication
+        title="Travel Log"
+        img={Assets.Tablet.travelLog}
+        on:applicationContainerEvent={handleAppContainerEvent} />
+    <SpotApplication
+        title="Profile"
+        img={Assets.Tablet.profile}
+        on:applicationContainerEvent={handleAppContainerEvent} />
+    <SpotApplication
+        title="Badges"
+        img={Assets.Tablet.badges}
+        on:applicationContainerEvent={handleAppContainerEvent} />
 </div>
 
 <style>
-    div {
-        background-color: rgba(0, 0, 0, 0.5);
-        color: white;
-        padding: 1rem;
-        border-radius: 1rem;
+    #tablet-menu {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
     }
+
 </style>
