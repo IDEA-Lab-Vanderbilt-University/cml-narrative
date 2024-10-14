94b56166fcce3db1b5e78f868e946cd7cf504da2
Gordon Stein <gstein@ltu.edu>
2024-10-13 21:39:33 -0500

    Make agent profiles in onboarding more accessible

--- src/lib/components/tablet/ClickToViewProfileBanner.svelte
+++ src/lib/components/tablet/ClickToViewProfileBanner.svelte
@@ -1,21 +1,15 @@
-<!--
- /src/lib/components/tablet/ClickToViewProfileBanner.svelte
- ClickToViewProfileBanner.svelte
- cml-narrative
- 
- Created by Ian Thompson on January 10th 2023
- icthomp@g.clemson.edu
- 
- https://idealab.sites.clemson.edu
- 
---->
+<script lang="ts">
+	export let handleClick: () => void;
+</script>
+
 
 <div
 	class="hud-red-blue-border bg-opacity-85 flex w-1/3 items-center justify-center space-x-5 rounded-md bg-slate-700 py-5 px-3 text-2xl text-white">
 	<!-- <img src="/img/characters/agent-spark/spark_gesture.png" alt="" class="h-24 -scale-x-1"> -->
 	<p>
 		Need ideas? Check out the SPOT agent's profiles and see words that describe them! <a
-			href="/introduction/onboarding/create-profile/agent-spark-profile"
+			href="#"
+			on:click={handleClick}
 			class="rounded-lg bg-gradient-to-b from-red-400 to-red-600 px-3 text-black shadow-xl"
 			>Profiles</a>
 	</p>
