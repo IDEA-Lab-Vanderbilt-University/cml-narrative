54073e5544c7b933b1a33ce44145fe9fe28a5b59
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2024-10-25 12:38:26 -0500

    Yes no buttons

--- src/routes/level2/car-training/+page.svelte
+++ src/routes/level2/car-training/+page.svelte
@@ -96,7 +96,27 @@
 
     let lineNumber = 1;
     $: lineNumber = line.id;
+
+	let choiceOptions = [
+		'Stop when people cross the street',
+		'Recognize human faces',
+		'Open doors for humans',
+		'Honk at birds',
+		'Recognize street signs',
+		'Open doors for all animals',
+	];
+
+	let choiceCorrect = [
+		true,
+		true,
+		true,
+		false,
+		true,
+		false,
+	]
 </script>
+
+<svelte:document />
 
 <Scene background={line.background} audio={line.audio}>
 	<div class="w-full" slot="dialog">
@@ -105,6 +125,55 @@
 	<div slot="content" class="content"  bind:this={content}>
 		{#if line.id == 1}
 			<img src="/img/misc/vroomlogo.png" alt="Vroom" style="width: 70%;" />
+		{/if}
+
+		{#if line.id == 2}
+			<div class="options">
+				<div class="option">
+					<div style="text-align: center; width: 2vw; font-size: 1.5vw;">
+						Yes 
+					</div>
+					<div style="text-align: center; width: 2vw; font-size: 1.5vw;">
+						No 
+					</div>
+					<div style="text-align: center; font-size: 1vw;">
+						Which of these do we need to<br/> train our cars to be able to do?
+					</div>
+				</div>
+				{#each choiceOptions as option, i}
+					<div class="option flex flex-row w-full">
+						<div>
+							<input type="radio" id={option} name={"choice" + i} value={option} />
+							<button class="radio" on:click={(e) => {
+								if (choiceCorrect[i]) {
+									e.target.style.backgroundColor = 'green';
+									document.getElementById("choice" + i + "_no").style.backgroundColor = '';
+								} else {
+									e.target.style.backgroundColor = 'red';
+									document.getElementById("choice" + i + "_no").style.backgroundColor = '';
+								}
+							}}
+							id={"choice" + i + "_yes"}>
+							</button>
+						</div>
+						<div>
+							<input type="radio" id={option} name={"choice" + i} value={option} />
+							<button class="radio" on:click={(e) => {
+								if (!choiceCorrect[i]) {
+									e.target.style.backgroundColor = 'green';
+									document.getElementById("choice" + i + "_yes").style.backgroundColor = '';
+								} else {
+									e.target.style.backgroundColor = 'red';
+									document.getElementById("choice" + i + "_yes").style.backgroundColor = '';
+								}
+							}}
+							id={"choice" + i + "_no"} >
+							</button>
+						</div>
+						<label for={"choice" + i}>{option}</label>
+					</div>
+				{/each}
+			</div>
 		{/if}
 	</div>
 </Scene>
@@ -121,5 +190,50 @@
 		top: 0;
 		right: 0;
 	}
+
+	.options {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: start;
+		gap: 1vh;
+		background-color: white;
+		padding: 1vh;
+		border: 0.2vw solid rgb(255, 150, 230);
+	}
+
+	.option {
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: start;
+		gap: 1vw;
+		width: 100%;
+	}
+
+	.option label {
+		font-size: 1.5vw;
+		color:  rgb(189, 75, 207);
+		border-bottom: 0.25vw solid black;
+	}
+
+	.option input[type="radio"] {
+		display: none;
+	}
+
+	.option input[type="radio"] + span {
+		width: 1.5vw;
+		height: 1.5vw;
+		border-radius: 50%;
+		border: 1px solid black;
+		display: inline-block;
+		position: relative;
+		left: 25%;
+	}
+
+	.option input[type="radio"]:checked + span {
+		background-color: black;
+	}
+
 
 </style>
