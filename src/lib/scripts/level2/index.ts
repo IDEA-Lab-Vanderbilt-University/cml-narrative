675c1326879a6bc2cf14620f451f01be5a87a793
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2024-11-04 12:43:05 -0600

    Add blank level 3 page

--- src/lib/scripts/level2/index.ts
+++ src/lib/scripts/level2/index.ts
@@ -1,32 +1,4 @@
 import type { Script } from '$lib/types/Script';
-import type { UserData } from '$lib/types/UserData';
-import { userDataStore } from '$lib/utils/stores/store';
-
-let agent: UserData = {
-	name: {
-		first: '',
-		last: ''
-	},
-	age: 0,
-	interests: [],
-	avatarImg: '',
-	agentName: '',
-	email: '',
-	password: '',
-	progress: {
-		level: 0,
-		levelLabel: '',
-		subLevel: 0,
-		subLevelLabel: '',
-		lastUpdated: undefined
-	}
-};
-
-userDataStore.subscribe((value) => {
-	agent = value as UserData;
-});
-
-
 
 const script: Script = {
 	lines: [
