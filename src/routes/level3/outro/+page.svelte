c0bcab1bd380b7efb216cce39ba1b5886bf98cf1
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2024-11-11 14:32:45 -0600

    placeholder level 3 outro

---
+++ src/routes/level3/outro/+page.svelte
@@ -0,0 +1,100 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import DialogBox from '$lib/components/dialog/DialogBox.svelte';
+	import Scene from '$lib/components/scene/Scene.svelte';
+	import TabletButton from '$lib/components/tablet/TabletButton.svelte';
+	import { NavigationDirection } from '$lib/types/Enums';
+	import type { Line } from '$lib/types/Script';
+	import type { UserProgress } from '$lib/types/UserData.js';
+	import DataService from '$lib/utils/DataService/index.js';
+	import { userDataStore } from '$lib/utils/stores/store.js';
+	import { createEventDispatcher } from 'svelte';
+	import script from '$lib/scripts/level2/index.js';
+	import Tablet from '$lib/components/tablet/Tablet.svelte';
+	import SpotApplication from '$lib/components/sequences/tablet/tablet-tutorial/SpotApplication.svelte';
+
+	export let data;
+
+	let line: Line;
+	$: line = data.line;
+
+    let lineNumber = 1;
+    $: lineNumber = line.id;
+
+	/**
+	 * Handles an emitted dialogEvent as sent from a DialogControl component and progresses the script as such
+	 * @param event can be destructured to obtain which way the dialog in a script should progress
+	 */
+	const handleDialogEvent = async (event: any) => {
+		var state: NavigationDirection = event.detail.state;
+
+		handleNavigation(state);
+	};
+
+	const getUpdatedProgress = ():UserProgress => {
+		return {
+			level: 0,
+			levelLabel: 'level-one',
+			subLevel: 0,
+			subLevelLabel: '/level1?page=1',
+			lastUpdated: new Date()
+		};
+	}
+
+	const updateLocalProgress = (progress: UserProgress) => {
+		userDataStore.update((data) => {
+			data.progress = progress;
+			return data;
+		})
+	}
+
+	/**
+	 * Determine the state of the DialogEvent that was emitted. Then, we will navigate
+	 * the user to the appropriate url with appropriate querystring which represents
+	 * which line in the script should be returned to the user.
+	 */
+	const handleNavigation = async (direction: NavigationDirection) => {
+		if (direction == NavigationDirection.forward) {
+			if (line.id == script.lines.length) {
+				let progress = getUpdatedProgress();
+				await DataService.Data.updateUserProgress(progress);
+				updateLocalProgress(progress);
+				
+                // Next level
+                goto('/level3/outro?page=1');
+			} else {
+				goto(`/level3?page=${line.id + 1}`);
+			}
+		} else if (direction == NavigationDirection.backward) {
+			if(line.id > 1) {
+				goto(`/level3?page=${line.id - 1}`);
+			} else {
+				goto('/level2/outro?page=9');
+			}
+		}
+	};
+
+    let content: HTMLDivElement | null;
+
+</script>
+
+<Scene background={line.background} audio={line.audio}>
+	<div class="w-full" slot="dialog">
+		<DialogBox {line} on:dialogEvent={handleDialogEvent} />
+	</div>
+
+	<div slot="content" class="h-full w-full" bind:this={content}>
+		<TabletButton on:click={() => { 
+            const event  = new CustomEvent('showTablet', {
+                bubbles: true
+            });
+            
+            content?.dispatchEvent(event);
+		}}
+		/>
+    </div>
+</Scene>
+
+<style>
+    
+</style>
---
+++ src/routes/level3/outro/+page.ts
@@ -0,0 +1,4 @@
+import script from '$lib/scripts/level3/outro/index';
+import { createPageLoad } from '$lib/utils/createPageLoad';
+
+export const load = createPageLoad(script);
