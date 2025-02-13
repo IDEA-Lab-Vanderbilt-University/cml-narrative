9f5757bda79056275aa7f7c83455689af69c71fc
Gordon Stein <gstein@ltu.edu>
2025-02-13 17:24:17 -0600

    Refactor out steps page

--- src/routes/level4/+page.svelte
+++ src/routes/level4/+page.svelte
@@ -19,6 +19,7 @@
 	import TextResponseModal from '$lib/components/activities/free-response/TextResponseModal.svelte';
 	import ImageResponse from '$lib/components/activities/free-response/ImageResponse.svelte';
 	import ImageResponseModal from '$lib/components/activities/free-response/ImageResponseModal.svelte';
+	import RobotStepsModal from '$lib/components/modals/RobotStepsModal.svelte';
 
 	export let data;
 
@@ -149,35 +150,7 @@
 		{/if}
 
 		{#if lineNumber == 9}
-			<Tablet showMeter={false} showBottomButtons={false}>
-				<div class="robostepsouter">
-					<h2>Your Prototype</h2>
-				</div>
-				<div class="robostepsouter">
-					<div class="robosteps">
-						<div class="robostep nextstep" on:click={() => goto('/level4?page=10')}>
-							<img src="/img/icons/robodesign.png"/>
-							<p>Design</p>
-						</div>
-						<div class="robostep">
-							<img src="/img/icons/roboplan.png"/>
-							<p>Plan</p>
-						</div>
-						<div class="robostep">
-							<img src="/img/icons/robotrain.png"/>
-							<p>Test & Train</p>
-						</div>
-						<div class="robostep">
-							<img src="/img/icons/roboprogram.png"/>
-							<p>Program</p>
-						</div>
-						<div class="robostep">
-							<img src="/img/icons/roboshowcase.png"/>
-							<p>Showcase</p>
-						</div>
-					</div>
-				</div>
-			</Tablet>
+			<RobotStepsModal currentStep={0} onStepClick={() => goto('/level4?page=10')} />
 		{/if}
 
 		{#if lineNumber == 10}
@@ -227,81 +200,6 @@
 </Scene>
 
 <style>
-	.robostepsouter {
-		background: linear-gradient(90deg, rgb(137, 200, 225), rgb(100, 159, 200));
-		width: 80%;
-		margin: 0 auto;
-		margin-bottom: 1vh;
-		border-radius: 5vh;
-		padding: 1.5vh;
-	}
-
-	.robostepsouter h2 {
-		text-align: center;
-		font-size: 6vh;
-		color: black;
-		font-weight: bold;
-	}
-
-    .robosteps {
-		display: flex;
-		flex-direction: column;
-		justify-content: space-around;
-		align-items: center;
-		height: 65vh;
-		background-color: white;
-		border-radius: 5vh;
-	}
-
-	.robostep {
-		background-color: peachpuff;
-		border-radius: 4vh;
-		height: 9vh;
-		width: 50%;
-	}
-
-	.robostep img {
-		height: 11vh;
-		position: relative;
-		top: -1vh;
-		left: -1vh;
-		clear: none;
-		float: left;
-	}
-
-	.robostep p {
-		line-height: 9vh;
-		position: relative;
-		left: 2vh;
-		font-size: 5vh;
-		text-shadow: 0 0 1vh rgba(0, 0, 0, 0.4);
-		user-select: none;
-	}
-
-	.robostep.donestep {
-		background-color: rgb(4, 159, 4);
-	}
-
-	.robostep.nextstep {
-		background-color: rgb(4, 159, 4);
-		cursor: pointer;
-		transition: all 0.3s;
-		animation: infinite 2.5s linear alternate pulse;
-	}
-
-	.robostep.nextstep:hover {
-		background-color: rgb(3, 103, 3);
-		transform: scale(1.05);
-	}
-
-	.robostep.nextstep:active {
-		transform: scale(0.95);
-	}
-
-	.robostep.nextstep p, .robostep.donestep p {
-		color: white;
-	}
-
 	.robostepintro {
 		width: 80%;
 		margin: 0 auto;
