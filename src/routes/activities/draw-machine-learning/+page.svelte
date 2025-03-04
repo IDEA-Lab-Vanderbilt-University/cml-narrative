cd2c290ff1c31119238651e23fa17954a6fb7a3f
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2025-03-04 10:27:49 -0600

    Upload images as travel logs

--- src/routes/activities/draw-machine-learning/+page.svelte
+++ src/routes/activities/draw-machine-learning/+page.svelte
@@ -20,35 +20,80 @@
 	}
 
 	const handleImageSubmission = async (event: CustomEvent<any>) => {
+		console.log(event);
 		const images: HTMLImageElement[] | HTMLOrSVGElement = event.detail.images;
 		doSubmit = event.detail.doSubmit;
-		let imageUrlsS3: string[] = [];
+		let imageUrls: string[] = [];
 		try {
+			//  Create a data URL from the images or SVG element
+			const canvas = document.createElement('canvas');
 			if (images instanceof Array) {
-				const promises = images.map(async (image) => {
-					let res = await DataService.Data.uploadImageOrSvgToS3(image.src, 'image');
-					return res;
-				});
-				imageUrlsS3 = await Promise.all(promises);
-				console.log('imageurls: ', imageUrlsS3);
-				await DataService.Data.uploadResponseImages('machineLearning', imageUrlsS3, 'image');
-				submissionType = 'image';
+				for(let image of images) {
+					if (image instanceof HTMLImageElement) {
+						// Check if image src is a data URL already
+						if (image.src.startsWith('data:')) {
+							imageUrls.push(image.src);
+							continue;
+						}
+
+						// Otherwise, draw the image on a canvas and get the data URL
+						canvas.width = image.width;
+						canvas.height = image.height;
+						const ctx = canvas.getContext('2d');
+
+						if (ctx) {
+							ctx.drawImage(image, 0, 0);
+							imageUrls.push(canvas.toDataURL('image/png'));
+						} else {
+							throw new Error('Failed to get canvas context');
+						}
+					}
+				}
+			} else if (images instanceof SVGElement) {
+				// Handle the SVG element
+				const svgData = new XMLSerializer().serializeToString(images);
+				const svgBlob = new Blob([svgData], { type: 'image/svg+xml;charset=utf-8' });
+				const url = URL.createObjectURL(svgBlob);
+				imageUrls.push(url);
+			} else if (images instanceof HTMLImageElement) {
+				canvas.width = images.width;
+				canvas.height = images.height;
+				const ctx = canvas.getContext('2d');
+
+				if (ctx) {
+					ctx.drawImage(images, 0, 0);
+					imageUrls.push(canvas.toDataURL('image/png'));
+				} else {
+					throw new Error('Failed to get canvas context');
+				}
 			} else {
-				let res = await DataService.Data.uploadImageOrSvgToS3(images, 'svg');
-				await DataService.Data.uploadResponseImages('machineLearning', res, 'svg');
-				submissionType = 'svg';
+				throw new Error('Invalid image type');
 			}
+
+			if (imageUrls.length === 0) {
+				throw new Error('No images to submit');
+			}
+
+			//  Submit the data URL to the server
+			await DataService.TravelLog.submitTravelLog(
+				{
+					description: 'draw-machine-learning',
+					data: imageUrls.length > 1 ? JSON.stringify(imageUrls) : imageUrls[0],
+					status: 'complete',
+				}
+			);
 			
-			message = `Response recorded successfully!`;
+			message = `Machine learning ${submissionType} responses were recorded successfully!`;
 			isSuccess = true;
 
 		} catch (error) {
-			message = `Responses submission failed!`;
-			isSuccess = false
-			console.error(`Error in submitting machine learning  ${submissionType} responses`, error);
+			message = `Machine learning ${submissionType} responses submission failed!`;
+			isSuccess = false;
+			console.error(error);
 		}
 		showFeedbackModal = true;
 	};
+
 	const onSubmit = () => {
 		studentProgressStore.update((progress) => {
 			progress.last_visited = '/training?page=12';
