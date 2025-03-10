341a4e97f92459c7e79f0c28063e7711138842d5
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2025-03-10 17:08:53 -0500

    Add level 3 outro voices

--- src/lib/scripts/level3/outro/index.ts
+++ src/lib/scripts/level3/outro/index.ts
@@ -8,7 +8,7 @@ const script: Script = {
             dialog: 'I feel good about this! I hope that changing the dataset to add MORE and DIFFERENT faces of kids will help the cars recognize kids\' faces and let them in!!!',
             avatars: ['/img/characters/bot-buddy/bot-buddy-happy-2.png'],
             background: '/img/backgrounds/level2/factory1.png',
-            audio: '/audio/level3/bot-buddy/bot-buddy-l3s1.wav',
+            audio: '/audio/level3/bot_buddy/bot_buddy_L3_S26.mp3',
         },
         {
             id: 2,
@@ -16,7 +16,7 @@ const script: Script = {
             dialog: 'Hello, Bot! Do you work here?',
             avatars: ['/img/characters/level-3/security-bot/security-bot-1.png', '/img/characters/bot-buddy/bot-buddy-ohno.png'],
             background: '/img/backgrounds/level2/factory1.png',
-            audio: '/audio/level3/security-bot/security-bot-l3s2.wav',
+            audio: '/audio/level3/security_bot/security_bot_L3_S27.mp3',
         },
         {
             id: 3,
@@ -24,7 +24,7 @@ const script: Script = {
             dialog: 'No... I... we were just leaving. Nothing to see here Security Bot!',
             avatars: ['/img/characters/level-3/security-bot/security-bot-1.png', '/img/characters/bot-buddy/bot-buddy-stop.png'],
             background: '/img/backgrounds/level2/factory1.png',
-            audio: '/audio/level3/bot-buddy/bot-buddy-l3s3.wav',
+            audio: '/audio/level3/bot_buddy/bot_buddy_L3_S28.mp3',
         },
         {
             id: 4,
@@ -32,7 +32,7 @@ const script: Script = {
             dialog: 'Agent, let’s go and find Kelvin and tell him what we learned!',
             avatars: ['/img/characters/level-3/security-bot/security-bot-1.png', '/img/characters/bot-buddy/bot-buddy-stop.png'],
             background: '/img/backgrounds/level2/factory1.png',
-            audio: '/audio/level3/bot-buddy/bot-buddy-l3s4.wav',
+            audio: '/audio/level3/bot_buddy/bot_buddy_L3_S29.mp3',
         },
         {
             id: 5,
@@ -40,7 +40,7 @@ const script: Script = {
             dialog: 'Hey! What are you guys doing here?',
             avatars: ['/img/characters/level-2/boy/boy_waiting-removebg-preview.png', '/img/characters/bot-buddy/bot-buddy-stop.png'],
             background: '/img/backgrounds/level3/2.png',
-            audio: '/audio/level3/kelvin/kelvin-l3s5.wav',
+            audio: '/audio/level3/kelvin/kelvin_L3_S30.mp3',
         },
         {
             id: 6,
@@ -48,7 +48,7 @@ const script: Script = {
             dialog: 'We have some good news, Kelvin! The training data images that Vroom Vroom used were mostly of adults-not kids like you. That\'s why so many kids couldn\'t use the cars.',
             avatars: ['/img/characters/level-2/boy/boy_waiting-removebg-preview.png', '/img/characters/bot-buddy/bot-buddy-happy-2.png'],
             background: '/img/backgrounds/level3/2.png',
-            audio: '/audio/level3/bot-buddy/bot-buddy-l3s6.wav',
+            audio: '/audio/level3/bot_buddy/bot_buddy_L3_S31.mp3',
         },
         {
             id: 7,
@@ -56,7 +56,7 @@ const script: Script = {
             dialog: 'We added MORE and DIFFERENT types of kid images to the training dataset to reduce BIAS. Now, the self-driving cars will recognize kids better!',
             avatars: ['/img/characters/level-2/boy/boy_waiting-removebg-preview.png', '/img/characters/bot-buddy/bot-buddy-happy-2.png'],
             background: '/img/backgrounds/level3/2.png',
-            audio: '/audio/level3/bot-buddy/bot-buddy-l3s7.wav',
+            audio: '/audio/level3/bot_buddy/bot_buddy_L3_S32.mp3',
         },
         {
             id: 8,
@@ -64,7 +64,7 @@ const script: Script = {
             dialog: 'We can never completely get rid of BIAS because we all have opinions that influence how we choose our datasets. So, we should be more mindful when we put together training datasets.',
             avatars: ['/img/characters/level-2/boy/boy_waiting-removebg-preview.png', '/img/characters/bot-buddy/bot-buddy-stop.png'],
             background: '/img/backgrounds/level3/2.png',
-            audio: '/audio/level3/bot-buddy/bot-buddy-l3s8.wav',
+            audio: '/audio/level3/bot_buddy/bot_buddy_L3_S33.mp3',
         },
         {
             id: 9,
@@ -72,7 +72,7 @@ const script: Script = {
             dialog: 'Wow! Thanks for helping us out. This solves one problem with our technologies. But we still have a lot of work to do to make sure people are treated fairly when designing technologies.',
             avatars: ['/img/characters/level-2/boy/boy_waiting-removebg-preview.png', '/img/characters/bot-buddy/bot-buddy-happy-2.png'],
             background: '/img/backgrounds/level3/2.png',
-            audio: '/audio/level3/kelvin/kelvin-l3s9.wav',
+            audio: '/audio/level3/kelvin/kelvin_L3_S34.mp3',
         },
         {
             id: 10,
@@ -81,7 +81,7 @@ const script: Script = {
                 'Incoming message from Mission Control!',
             avatars: ['/img/characters/level-1/spot-computer.png'],
             background: '/img/backgrounds/level1/cafe.png',
-            audio: '/audio/level1/computer_voice/Computer_voice_L1S33.wav',
+            audio: '/audio/level3/computer_voice/computer_voice_L3_S35.mp3',
             pos: 'left',
         },
         {
@@ -91,7 +91,7 @@ const script: Script = {
                 'Your Bot Buddy’s system has informed us that you have completed your third mission. Mission Control needs to know a few more things. We have included our questions in this message.',
             avatars: ['/img/characters/captain-storm/storm_yeah.png'],
             background: '/img/backgrounds/level1/cafe.png',           
-            audio: '/audio/level3/computer_voice/Computer_voice_L1S34.wav',
+            audio: '/audio/level3/computer_voice/computer_voice_L3_S36.mp3',
             pos: 'left',
         },
         {
