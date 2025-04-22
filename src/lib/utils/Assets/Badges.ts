5acba905eaa32cab55bca76b5d84f5a80d7af4fa
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2025-04-22 17:13:04 -0500

    update badges

--- src/lib/utils/Assets/Badges.ts
+++ src/lib/utils/Assets/Badges.ts
@@ -8,31 +8,31 @@ export type BadgeType = {
 export const Badges: BadgeType[] = [
     {
         name: "Agent-In-Training", 
-        image: "/img/misc/badge1.png",
+        image: "/img/misc/badge1new.png",
         description: "You've joined the SPOT agency! You are now an Agent-In-Training!",
         count: 1,
     },
     {
         name: "Algorithm All Stars",
-        image: "/img/misc/badge2.png",
+        image: "/img/misc/badge1new.png",
         description: "You've learned about algorithms and how they work! You are now an Algorithm All Star!",
         count: 2,
     },
     {
         name: "Machine Learning Master",  
-        image: "/img/misc/badge3.png",
+        image: "/img/misc/badgemaster.png",
         description: "You've trained and tested a machine learning model! You are now a Machine Learning Master!",
         count: 3,
     },
     {
         name: "Bias Buster", 
-        image: "/img/misc/badge4.png",
+        image: "/img/misc/badge2new.png",
         description: "You've learned about bias and how to avoid it! You are now a Bias Buster!",
         count: 4,
     },
     {
         name: "Junior Agent",
-        image: "/img/misc/badge5.png", 
+        image: "/img/misc/badge2new.png",
         description: "You've returned from your first mission! You are now a Junior Agent!",
         count: 5,
     },
