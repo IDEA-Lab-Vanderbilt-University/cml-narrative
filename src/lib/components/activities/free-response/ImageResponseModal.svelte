6a489a08796c568adbe868fbf546627890f14090
Gordon Stein <7331488+gsteinLTU@users.noreply.github.com>
2025-07-31 14:36:14 -0500

    Add voiceover for text/image response

--- src/lib/components/activities/free-response/ImageResponseModal.svelte
+++ src/lib/components/activities/free-response/ImageResponseModal.svelte
@@ -1,5 +1,6 @@
 
 <script lang="ts">
+	import AudioPlayer from "$lib/components/audio/AudioPlayer.svelte";
 	import FeedbackModal from "$lib/components/modals/FeedbackModal.svelte";
 	import Tablet from "$lib/components/tablet/Tablet.svelte";
 	import ImageResponse from "./ImageResponse.svelte";
@@ -16,9 +17,23 @@
     if (promptedTechnology !== '') {
         prompt = 'Draw a picture of what you think ' + promptedTechnology + ' is.';
     }
+
+    let audio = '';
+
+    if (promptedTechnology) {
+        if (promptedTechnology.toLowerCase() == 'an algorithm') {
+            audio = '/audio/level0/L0-S94.mp3';
+        } else if (promptedTechnology.toLowerCase() == 'machine learning') {
+            audio = '/audio/level0/L0-S99.mp3';
+        }
+    }
 </script>
 
 <Tablet showMeter={false}>
+    {#if audio}
+        <AudioPlayer src={audio} />
+    {/if}
+
 	{#if showFeedbackModal}
 		<FeedbackModal {message} {isSuccess} on:close={onFeedbackClose} />
 	{/if}
